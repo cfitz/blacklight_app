a032be3b93a7cc7d6ed10391f7694f1485ce85de
chrisfitzpatrick <chrisfitzpatrick@chrisfitzpatricks-MacBook-Air.local>
2013-05-12 22:49:13 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlacklightApp::Application.config.secret_token = '079fbd1145346422582b021f629ee713a11d096457c915e8469591bb62a98970cccaecdd18946dab15c30169e4feb9194c0b719816fe879ac9b4b729cabca73f'
